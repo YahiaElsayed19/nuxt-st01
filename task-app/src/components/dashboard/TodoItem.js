a7063c9f532d18c19f5fe031525808e5f35ca620
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-03-23 03:56:56 +0200

    completed toggle done

--- task-app/src/components/dashboard/TodoItem.js
+++ task-app/src/components/dashboard/TodoItem.js
@@ -1,22 +1,18 @@
 import React, { useContext, useState } from 'react'
 import { AuthContext } from '../../store/auth-ctx'
-import { toggleTodo } from '../../util/todos'
 import classes from './TodoItem.module.css'
 
-const TodoItem = ({ task, completed, id }) => {
-    const [edit, setEdit] = useState(false)
+const TodoItem = ({ task, completed, id, onCompleteHandler }) => {
     const authCtx = useContext(AuthContext)
     let taskClasses = `${classes.task} ${completed && classes.completed}`
-    const onCompleteHandler = () => {
-        toggleTodo(authCtx.selectedAccount.username, authCtx.selectedAccount.password, id)
-    }
+
     return (
         <li className={taskClasses}>
             <div>
                 {task}
             </div>
             <div className={classes.actions}>
-                <button className={classes.action} onClick={onCompleteHandler}>Completed</button>
+                <button className={classes.action} onClick={onCompleteHandler.bind(this, id)}>Completed</button>
                 <button className={classes.action}>Delete</button>
             </div>
         </li>
--- task-app/src/components/dashboard/TodoList.js
+++ task-app/src/components/dashboard/TodoList.js
@@ -4,22 +4,27 @@ import { AuthContext } from '../../store/auth-ctx'
 import { getTodosList } from '../../util/todos'
 import AddTask from './AddTask'
 import TodoItem from './TodoItem'
+import { toggleTodo } from '../../util/todos'
 import classes from './TodoList.module.css'
 function TodoList() {
     const [add, setAdd] = useState(false)
     const authCtx = useContext(AuthContext)
     const username = authCtx.selectedAccount.username
     const password = authCtx.selectedAccount.password
-    const { data } = useQuery("GetTodos", () => getTodosList(username, password))
+    const { data, refetch } = useQuery("GetTodos", () => getTodosList(username, password))
     const todos = data?.data
     const addHandler = () => {
         setAdd((prev) => !prev)
+    }
+    const onCompleteHandler = (id) => {
+        toggleTodo(authCtx.selectedAccount.username, authCtx.selectedAccount.password, id)
+        refetch()
     }
     return (
         <div className={classes.todos}>
             <div className={classes.container}>
                 <ul className={classes.list}>
-                    {todos?.map(todo => <TodoItem key={todo.id} id={todo.id} task={todo.task} completed={todo.completed} />)}
+                    {todos?.map(todo => <TodoItem key={todo.id} id={todo.id} task={todo.task} completed={todo.completed} onCompleteHandler={onCompleteHandler} />)}
                 </ul>
                 {add && <AddTask />}
                 <button className={classes['add-btn']} onClick={addHandler}>{add ? "Cancel" : "Add"}</button>
