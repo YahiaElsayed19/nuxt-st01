82090d3fb8697cb40cc9b9e87ed3c15f23ca3de2
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-03-23 06:03:32 +0200

    add auto focus

--- task-app/src/components/Input.js
+++ task-app/src/components/Input.js
@@ -1,15 +1,22 @@
-import React, { useContext, useRef } from 'react'
-import { AuthContext } from '../store/auth-ctx'
-import classes from './Input.module.css'
+import React, { useContext, useRef } from "react";
+import { AuthContext } from "../store/auth-ctx";
+import classes from "./Input.module.css";
 function Input() {
-    const passwordRef = useRef()
-    const authCtx = useContext(AuthContext)
+    const passwordRef = useRef();
+    const authCtx = useContext(AuthContext);
     const onEnterPassword = () => {
-        authCtx.setEnterdPassword(passwordRef.current.value)
-    }
+        authCtx.setEnterdPassword(passwordRef.current.value);
+    };
     return (
-        <input type="password" className={classes.input} placeholder="enter your password" ref={passwordRef} onChange={onEnterPassword} />
-    )
+        <input
+            type="password"
+            className={classes.input}
+            placeholder="enter your password"
+            ref={passwordRef}
+            onChange={onEnterPassword}
+            autoFocus
+        />
+    );
 }
 
-export default Input
+export default Input;
